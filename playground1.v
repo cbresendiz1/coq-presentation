12232a3084b8a51b8e452eeffad3920a2b7d77f7
cbresendiz1 <cbresendiz1@gmail.com>
2018-05-29 09:21:16 -0400

    My addition

--- playground1.v
+++ playground1.v
@@ -177,3 +177,4 @@ Inductive even : nat -> Prop :=
 | S_of_odd_is_even : (forall n : nat, odd n -> even (S n))
 with odd : nat -> Prop :=
 | S_of_even_is_odd : (forall n : nat, even n -> odd (S n)).
+
